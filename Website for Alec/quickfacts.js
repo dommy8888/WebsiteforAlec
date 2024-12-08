d32aeb210142adc63b1a27ca688e63ae66ba755d
dommy8888 <dominick.castillo08@yahoo.com>
2024-12-08 13:00:31 -0600

    Delete Website for Alec directory

--- Website for Alec/quickfacts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("dommyBtn").addEventListener("click", function() {
-    document.getElementById("factWall").style.display = "block";
-});
-
-document.getElementById("alecBtn").addEventListener("click", function() {
-    document.getElementById("factWall").style.display = "block";
-});
